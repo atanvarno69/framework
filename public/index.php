1d7d29fc56a067fb085822517eef611b9181a02a
atanvarno <atanvarno69@gmail.com>
2017-01-03 16:22:32 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
+/**
+ * index.php
+ *
+ * Redirects to /src/main.php.
+ *
+ * @package   Atan\Framework
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'main.php';
